4ff71420f90833a3078c0a2c7db0838c9e25ff1e
Greg Manrodt <gmanrodt@rbaconsulting.com>
2022-11-28 15:53:49 -0600

    another fix

--- src/project/web/nk-store2/components/nav.tsx
+++ src/project/web/nk-store2/components/nav.tsx
@@ -2,7 +2,7 @@ export default function Nav(){
     return <nav id="header" className="w-full z-30 top-0 py-1">
     <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-6 py-3">
 
-        <label for="menu-toggle" className="cursor-pointer md:hidden block">
+        <label htmlFor="menu-toggle" className="cursor-pointer md:hidden block">
             <svg className="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
                 <title>menu</title>
                 <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
