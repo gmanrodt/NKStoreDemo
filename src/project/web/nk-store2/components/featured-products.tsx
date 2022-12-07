ffe62116ddd37afed22a2cf1f3946af106da9c6a
Greg Manrodt <gmanrodt@rbaconsulting.com>
2022-12-07 11:22:27 -0600

    Another update to the products page

--- src/project/web/nk-store2/components/featured-products.tsx
+++ src/project/web/nk-store2/components/featured-products.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function FeaturedProducts({ products, heading }: any) {
   return (
@@ -6,17 +7,14 @@ export default function FeaturedProducts({ products, heading }: any) {
       <div className="container mx-auto flex items-center flex-wrap pt-4 pb-12">
         <nav id="store" className="w-full z-30 top-0 px-6 py-1">
           <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-3">
-            <a
-              className="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl "
-              href="#"
-            >
+              <div className="items-left uppercase text-xl">
               {heading}
-            </a>
+              </div>
 
-            <div className="flex items-center" id="store-nav-content">
-              <a
+            <div className="flex items-right" id="store-nav-content">
+              <Link
                 className="pl-3 inline-block no-underline hover:text-black"
-                href="#"
+                href="/filter"
               >
                 <svg
                   className="fill-current hover:text-black"
@@ -27,11 +25,11 @@ export default function FeaturedProducts({ products, heading }: any) {
                 >
                   <path d="M7 11H17V13H7zM4 7H20V9H4zM10 15H14V17H10z" />
                 </svg>
-              </a>
+              </Link>
 
-              <a
+              <Link
                 className="pl-3 inline-block no-underline hover:text-black"
-                href="#"
+                href="/search"
               >
                 <svg
                   className="fill-current hover:text-black"
@@ -42,7 +40,7 @@ export default function FeaturedProducts({ products, heading }: any) {
                 >
                   <path d="M10,18c1.846,0,3.543-0.635,4.897-1.688l4.396,4.396l1.414-1.414l-4.396-4.396C17.365,13.543,18,11.846,18,10 c0-4.411-3.589-8-8-8s-8,3.589-8,8S5.589,18,10,18z M10,4c3.309,0,6,2.691,6,6s-2.691,6-6,6s-6-2.691-6-6S6.691,4,10,4z" />
                 </svg>
-              </a>
+              </Link>
             </div>
           </div>
         </nav>
@@ -51,7 +49,7 @@ export default function FeaturedProducts({ products, heading }: any) {
             className="w-full l:w-1/3 md:w-1/2 xl:w-1/4 p-6 flex flex-col"
             key={p.codename}
           >
-            <a href={p.productUrl}>
+            <Link href={p.productUrl}>
               <div className="image-container">
                 <Image
                   className="image hover:grow hover:shadow-lg"
@@ -72,7 +70,7 @@ export default function FeaturedProducts({ products, heading }: any) {
                 </svg>
               </div>
               <p className="pt-1 text-gray-900">${p.price}</p>
-            </a>
+            </Link>
           </div>
         ))}
       </div>
