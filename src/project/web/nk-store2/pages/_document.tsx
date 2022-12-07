4e95c29c193a2208cae0127bd2321b8c1e71094a
Greg Manrodt <gmanrodt@rbaconsulting.com>
2022-12-07 11:03:42 -0600

    Updated links to use Link instead of <a>

--- src/project/web/nk-store2/pages/_document.tsx
+++ src/project/web/nk-store2/pages/_document.tsx
@@ -15,7 +15,6 @@ export default function Document() {
       <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <title>NK Shop</title>
         <meta name="description" content="description" />
         <meta name="keywords" content="tailwind,tailwindcss,tailwind css,css,starter template,free template,store template, shop layout, minimal, monochrome, minimalistic, theme, nordic" />
         <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css"/>
