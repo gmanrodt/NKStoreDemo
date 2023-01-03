9939264c96cc208c9e3603d29119ec2ee320d46a
Greg Manrodt <greg.manrodt@gmail.com>
2023-01-03 13:24:41 -0600

    Update _app.tsx

--- src/project/web/nk-store2/pages/_app.tsx
+++ src/project/web/nk-store2/pages/_app.tsx
@@ -5,21 +5,7 @@ import Script from "next/script";
 export default function App({ Component, pageProps }: AppProps) {
   return (
       <>
-        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-MVLTHPR40S"></Script >
-        <Script
-         id="google-analytics"
-         strategy="afterInteractive"
-         dangerouslySetInnerHTML={{
-           __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-             gtag('js', new Date());
-             gtag('config', 'G-MVLTHPR40S', {
-              page_path: window.location.pathname,
-             });
-            `,
-         }} />
-	      <Component {...pageProps} />
+       	<Component {...pageProps} />
        </>
         );
 }
